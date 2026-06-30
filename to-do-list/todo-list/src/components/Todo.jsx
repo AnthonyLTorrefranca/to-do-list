9b39f300bc0222a441a0955f3e45dc6f14f1f0c3
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-30 18:28:37 +0800

    feat: added map and function that creates element when submitted

--- to-do-list/todo-list/src/components/Todo.jsx
+++ to-do-list/todo-list/src/components/Todo.jsx
@@ -2,20 +2,41 @@ import React, { useState } from 'react'
 import './Todo.css'
 
 const Todo = () => {
-    const [todo, setTodo] = useState("")
+    const [newTask, setNewTask] = useState("")
+    const [taskHandler, setTaskHandler] = useState([])
+    const handleGet = (e) => {
+      setNewTask(e.target.value)
+    }
     function handleAdd(e){
         e.preventDefault()
-        // console.log(object)
+        console.log(newTask)
+        if (newTask.trim() === "") { 
+          alert("New task needs to contain sometin") 
+          return
+        }  
+        setTaskHandler(prev => ([...prev, newTask]))
+        setNewTask("")
     }
-  return (
-    <section>
-        <form className="todo_container" onSubmit={handleAdd}>
-            <input type="text" placeholder='Type a new task!' className="todo_input" />
+return (
+  <section>
+      <form className="todo_container" onSubmit={handleAdd}>
+        <h2>Welcome back! Add your new task.</h2>
+          <section className="add-task">
+            <input id='createTask' type="text" 
+              placeholder='Type a new task!'
+              className="todo_input" value={newTask}
+              name='newTask' onChange={handleGet} />
             <button type='submit'>ADD</button>
-        </form>
-      <h1></h1>
+          </section>
+      </form>
+    <section className="taskList">
+      {taskHandler.map((task, index)=>(
+        <ul id={index}>
+          <li key={index} className='tasks'>{task}</li>
+        </ul>
+      ))}
     </section>
-  )
-}
+  </section>
+)}
 
 export default Todo
