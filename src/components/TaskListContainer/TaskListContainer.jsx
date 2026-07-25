f9a298fdb203684de249ac91c1e9f87b1e445ace
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-07-25 20:58:33 +0800

    feat: added edit task while checking if the edited task exist.

--- src/components/TaskListContainer/TaskListContainer.jsx
+++ src/components/TaskListContainer/TaskListContainer.jsx
@@ -14,37 +14,39 @@ export default function TaskListContainer() {
         const selTaskInd = taskList.findIndex(item=> item.id === index);
         const selTask = taskList[selTaskInd].text
         setTask(selTask)
-    } // handles the submit
+        setEditTask(null)
+    }
+    // handles the submit
     function handleSubmit(e){
         e.preventDefault()
         // gives new task unique id
         const newTask = { id: crypto.randomUUID(), text: task}
+
         // checks the task if it's blank
         if (task.trim() === ""){
             setAlert("blank");
             return
         }
+        if (editTask !== null){
+            setEditTask("edit")
+            setTaskList(prevTask=> 
+                prevTask.map(item=> item.id === editTask ?
+                    {...item, text: task} : item ))
+            return
+        }
+
         // checks duplicate for both old and new task
         if (taskList.some(item=> item.text === newTask.text)){
             console.log("dup check")
             setAlert("duplicate");
             return
         }
-        // handles the edit
-        if (editTask !== null){
-            setTask("")
-            setTaskList(prevTask=> 
-                prevTask.map(item=> item.id === editTask ?
-                    {...item, text: task} : item
-                )
-            )
-            setEditTask(null)
-            return
-        }
         // appends task to the list
         setTaskList(prev=> [...prev, newTask])
+        console.log(taskList)
         setTask("")
-    } // handles the changes on input 
+    } 
+    // handles the changes on input 
     function handleChange(e){
         setTask(e.target.value)
         setAlert("idle")
@@ -92,8 +94,9 @@ return (
     <>
     <section className="flex flex-col items-center border rounded-3xl h-350 w-150 px-30 pt-10 overflow-hidden">
         <TaskAlert alert={alert} />
-        <TaskInput task={task} handleChange={handleChange} handleSubmit={handleSubmit} />
-        <TaskLists taskList={taskList} handleDelete={handleDelete} handleMoveUp={handleMoveUp} handleMoveDown={handleMoveDown} handleEdit={handleEdit} />
+        <TaskInput task={task} handleChange={handleChange} handleSubmit={handleSubmit} editTask={editTask} />
+        <TaskLists taskList={taskList} handleDelete={handleDelete} handleMoveUp={handleMoveUp} 
+            handleMoveDown={handleMoveDown} handleEdit={handleEdit} />
     </section>
     </>
   )
